25a6bf500711f95c1f644613beff5a144d3bb85d
fcsamuel <felipecarvalhosamuel@gmail.com>
2019-10-27 19:57:32 -0300

    Alterações iniciais no componente da ordem de serviço

--- WebApp/src/app/cadastros/ordem-servico/ordem-servico.component.ts
+++ WebApp/src/app/cadastros/ordem-servico/ordem-servico.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdemServico } from '../models/ordemServico';
+import { OrdemServicoService } from './ordem-servico.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-ordem-servico',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemServicoComponent implements OnInit {
 
-  constructor() { }
+  ordemServico: OrdemServico;
+  edit: boolean;
+
+  minDate = new Date();
+  date = new FormControl(new Date());
+
+  constructor(private ordemServicoService: OrdemServicoService,
+    public activatedRoute: ActivatedRoute,
+    public router: Router,
+    public spinner: NgxSpinnerService) { }
 
   ngOnInit() {
+    this.ordemServico = new OrdemServico();
+    this.activatedRoute.params.subscribe(
+      params =>  {
+        if (params.id != undefined) {
+          this.getById(params.id);
+          this.edit = true;
+        }
+      }
+    )
+  }
+
+  getById(id: any) {
+    this.ordemServicoService.list(id).subscribe( sucesso => {
+      if(sucesso != null) {
+        this.fill(sucesso);
+      }
+    }, error => {
+      console.log("Erro - getById");
+    })
+  }
+  fill(ordemServico: any) {
+    this.ordemServico = ordemServico;
   }
 
 }
