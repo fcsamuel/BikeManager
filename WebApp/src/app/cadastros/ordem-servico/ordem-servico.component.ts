dcb92392162af78f33a2dc16f75b40993dac9384
fcsamuel <felipecarvalhosamuel@gmail.com>
2019-11-03 11:55:14 -0300

    Parte da ordem de serviço implementada

--- WebApp/src/app/cadastros/ordem-servico/ordem-servico.component.ts
+++ WebApp/src/app/cadastros/ordem-servico/ordem-servico.component.ts
@@ -4,6 +4,8 @@ import { OrdemServicoService } from './ordem-servico.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { FormControl } from '@angular/forms';
+import { ClienteFornecedor } from '../models/clienteFornecedor';
+import { ClienteFornecedorService } from '../cliente-fornecedor/cliente-fornecedor.service';
 
 @Component({
   selector: 'app-ordem-servico',
@@ -14,6 +16,7 @@ export class OrdemServicoComponent implements OnInit {
 
   ordemServico: OrdemServico;
   edit: boolean;
+  clienteList: Array<ClienteFornecedor> = new Array<ClienteFornecedor>();
 
   minDate = new Date();
   date = new FormControl(new Date());
@@ -21,18 +24,26 @@ export class OrdemServicoComponent implements OnInit {
   constructor(private ordemServicoService: OrdemServicoService,
     public activatedRoute: ActivatedRoute,
     public router: Router,
-    public spinner: NgxSpinnerService) { }
+    public spinner: NgxSpinnerService,
+    public clienteService: ClienteFornecedorService) { }
 
   ngOnInit() {
     this.ordemServico = new OrdemServico();
+    this.loadClienteList();
     this.activatedRoute.params.subscribe(
       params =>  {
         if (params.id != undefined) {
           this.getById(params.id);
           this.edit = true;
         }
+        
       }
     )
+  }
+
+  backwards() {
+
+
   }
 
   getById(id: any) {
@@ -41,11 +52,26 @@ export class OrdemServicoComponent implements OnInit {
         this.fill(sucesso);
       }
     }, error => {
-      console.log("Erro - getById");
+      console.log("Ocorreu um erro no método getById(id: any) - ordem-servico.component.ts");
     })
   }
+
   fill(ordemServico: any) {
     this.ordemServico = ordemServico;
+  }
+
+  loadClienteList() {
+    this.spinner.show();
+    this.clienteService.listAll().subscribe(sucesso => {
+      if (sucesso != null) {
+        this.clienteList = sucesso;
+        this.spinner.hide();  
+        console.log(this.clienteList.length);
+      }
+    },
+    error => {
+      this.spinner.hide();
+    })
   }
 
 }
