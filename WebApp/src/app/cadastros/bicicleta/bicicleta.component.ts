de6f108d275cc411f4c79b9ce9b32f0b83a960f9
fcsamuel <felipecarvalhosamuel@gmail.com>
2019-11-10 18:35:17 -0300

    Componente bicicleta criado

---
+++ WebApp/src/app/cadastros/bicicleta/bicicleta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bicicleta',
+  templateUrl: './bicicleta.component.html',
+  styleUrls: ['./bicicleta.component.css']
+})
+export class BicicletaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
