7cf751abd33b73ca7c824b8fe468f05e6ea6868a
fcsamuel <felipecarvalhosamuel@gmail.com>
2019-11-15 15:26:17 -0300

    html da lista clientefornecedor implementado

--- WebApp/src/app/cadastros/cliente-fornecedor/cliente-fornecedor-list/cliente-fornecedor-list.component.ts
+++ WebApp/src/app/cadastros/cliente-fornecedor/cliente-fornecedor-list/cliente-fornecedor-list.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteFornecedorListComponent implements OnInit {
 
+  dataSource: any;
+
+  displayedColumns: string[] = ['cdClienteFornecedor', 'nrCpfCnpj', 'dsNomeRazao', 'dsFantasia', 'editColumn'];
+
   constructor() { }
 
   ngOnInit() {
